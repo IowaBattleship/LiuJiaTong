0010a1d725b55be15f7fb94bb4bad8798cc9cd68
913887524@qq.com <913887524@qq.com>
2023-05-11 23:34:22 +0800

    nt version handler and fix some stupid bugs

--- client/playing_handler.py
+++ client/playing_handler.py
@@ -23,9 +23,10 @@ def show_playingcards(
     # 最后刷新print的缓冲区
     sys.stdout.flush()
     
-class Direction(Enum):
-    left = 0
-    right = 1
+class Special_Input(Enum):
+    left_arrow = 0
+    right_arrow = 1
+    backspace = 2
 
 class InputException(Exception):
     def __init__(self, value):
@@ -40,91 +41,124 @@ class InputException(Exception):
 # 输出新的now_played_cards, cursor二元组
 if os.name == 'posix':
     # linux & mac
-    def read_userinput(
-        now_played_cards,
-        cursor: int,
-        user_cards
-    ):
-        def read_byte() -> str:
-            return sys.stdin.read(1)
+    def read_byte() -> str:
+        return sys.stdin.read(1)
 
-        def read_direction():
-            if read_byte() != '[':
-                raise InputException('(非法输入)')
-            else:
-                dir = read_byte()
-                if dir == 'D':
-                    return Direction.left
-                elif dir == 'C':
-                    return Direction.right
-                else:
-                    raise InputException('(非法输入)')
-        
-        def read_input():
-            fst_byte = read_byte().upper()
-            if fst_byte == '\x1b':
-                # 判断是否是左右方向键
-                return read_direction()
-            elif fst_byte in ['\n', '\t', '\x7f', 'C', 'F']:
-                return fst_byte
-            elif utils.str_to_int(fst_byte) != -1:
-                return fst_byte
+    def read_direction():
+        if read_byte() != '[':
+            raise InputException('(非法输入)')
+        else:
+            dir = read_byte()
+            if dir == 'D':
+                return Special_Input.left_arrow
+            elif dir == 'C':
+                return Special_Input.right_arrow
             else:
                 raise InputException('(非法输入)')
-
-        while True:
-            assert(cursor <= len(now_played_cards))
-            end_input = False
-            try:
-                input = read_input()
-                if input == Direction.left:
-                    if cursor > 0:
-                        cursor -= 1
-                elif input == Direction.right:
-                    if cursor < len(now_played_cards):
-                        cursor += 1
-                elif input == '\n':
-                    end_input = True
-                elif input == '\t':
-                    if cursor > 0:
-                        fill_char = now_played_cards[cursor - 1]
-                        used_num = now_played_cards.count(fill_char)
-                        total_num = user_cards.count(fill_char)
-                        fill_num = total_num - used_num
-                        now_played_cards = now_played_cards[:cursor] + fill_num * [fill_char] + now_played_cards[cursor:]
-                        cursor += fill_num
-                elif input == '\x7f': #退格
-                    if cursor > 0:
-                        now_played_cards = now_played_cards[:cursor - 1] + now_played_cards[cursor:]
-                        cursor -= 1
-                elif input == 'F':
-                    now_played_cards = ['F']
-                    cursor = 1
-                elif input == 'C':
-                    now_played_cards = []
-                    cursor = 0
-                else:
-                    assert(now_played_cards.count(input) <= user_cards.count(input))
-                    if now_played_cards.count(input) == user_cards.count(input):
-                        raise InputException('(你打出的牌超过上限了)')
-                    now_played_cards = now_played_cards[:cursor] + [input] + now_played_cards[cursor:]
-                    cursor += 1
-            except InputException as err:
-                show_playingcards(now_played_cards, cursor, err.args[0])
-            else:
-                show_playingcards(now_played_cards, cursor)
-            if end_input:
-                break
-        
-        return now_played_cards, cursor
+    
+    def read_input():
+        fst_byte = read_byte().upper()
+        if fst_byte == '\x1b':
+            # 判断是否是左右方向键
+            return read_direction()
+        elif fst_byte == '\x7f':
+            return Special_Input.backspace
+        elif fst_byte in ['\n', '\t', 'C', 'F']:
+            return fst_byte
+        elif utils.str_to_int(fst_byte) != -1:
+            return fst_byte
+        else:
+            raise InputException('(非法输入)')
+    
 elif os.name == 'nt':
     # windows
-    def read_userinput(
-        now_played_cards,
-        cursor: int,
-        user_cards
-    ):
-        pass
+    from msvcrt import getch
+    def read_byte() -> str:
+        char = chr(getch()[0])
+        if char == '\x03': # ctrl + C
+            raise KeyboardInterrupt()
+        return char
+        
+    def read_direction():
+        dir = read_byte()
+        if dir == 'K':
+            return Special_Input.left_arrow
+        elif dir == 'M':
+            return Special_Input.right_arrow
+        else:
+            raise InputException('(非法输入)')
+    
+    def read_input():
+        fst_byte = read_byte()
+        if fst_byte == '\xe0':
+            return read_direction()
+        fst_byte = fst_byte.upper()
+        if fst_byte == '\x08':
+            return Special_Input.backspace
+        elif fst_byte in ['\r', '\t', 'C', 'F']:
+            return fst_byte
+        elif utils.str_to_int(fst_byte) != -1:
+            return fst_byte
+        else:
+            raise InputException('(非法输入)')
+
+def read_userinput(
+    now_played_cards,
+    cursor: int,
+    user_cards
+):
+    while True:
+        assert(cursor <= len(now_played_cards))
+        end_input = False
+        try:
+            input = read_input()
+            if input == Special_Input.left_arrow:
+                if cursor > 0:
+                    cursor -= 1
+            elif input == Special_Input.right_arrow:
+                if cursor < len(now_played_cards):
+                    cursor += 1
+            elif input == Special_Input.backspace:
+                if cursor > 0:
+                    now_played_cards = now_played_cards[:cursor - 1] + now_played_cards[cursor:]
+                    cursor -= 1
+            elif input in ['\n', '\r']:
+                end_input = True
+            elif input == '\t':
+                if now_played_cards == ['F']:
+                    continue
+                if cursor > 0:
+                    fill_char = now_played_cards[cursor - 1]
+                    used_num = now_played_cards.count(fill_char)
+                    total_num = user_cards.count(fill_char)
+                    fill_num = total_num - used_num
+                    assert (fill_num >= 0)
+                    now_played_cards = now_played_cards[:cursor] + fill_num * [fill_char] + now_played_cards[cursor:]
+                    cursor += fill_num
+            elif input == 'F':
+                now_played_cards = ['F']
+                cursor = 1
+            elif input == 'C':
+                now_played_cards = []
+                cursor = 0
+            else:
+                assert(now_played_cards.count(input) <= user_cards.count(input))
+                if now_played_cards.count(input) == user_cards.count(input):
+                    raise InputException('(你打出的牌超过上限了)')
+                if now_played_cards == ['F']:
+                    now_played_cards = []
+                    cursor = 0
+                now_played_cards = now_played_cards[:cursor] + [input] + now_played_cards[cursor:]
+                cursor += 1
+        except InputException as err:
+            show_playingcards(now_played_cards, cursor, err.args[0])
+        else:
+            show_playingcards(now_played_cards, cursor)
+        if end_input:
+            break
+        assert(now_played_cards == ['F'] or now_played_cards.count('F') == 0)
+    
+    return now_played_cards, cursor
 
 # user: 用户信息
 # last_user: 最后打出牌的用户
