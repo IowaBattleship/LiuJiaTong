92da5ec977b0584b3c3782935a2eab9d830d676e
MrRedhat <callmeredhat@163.com>
2023-07-11 23:07:51 +0800

    Add test cases in test_judge_and_transform_cards

--- test/test_playingrules.py
+++ test/test_playingrules.py
@@ -6,7 +6,10 @@ sys.path.append("..")
 from client.playingrules import *
 
 @pytest.mark.parametrize('user_input, expect', [
-    [[15, 15, 15, 15], (1, 15)],
+    [[15, 15, 15, 15], (1, 15)], # test normal bomb
+    [[16, 16, 16, 16], (2, 16)], # test black joker bomb
+    [[17, 17, 17, 17], (3, 17)], # test red joker bomb
+    [[3, 4, 5, 6, 7],  (4, 7)],  # test straight
 ])
 def test_judge_and_transform_cards(user_input, expect):
     assert judge_and_transform_cards(user_input)==expect
